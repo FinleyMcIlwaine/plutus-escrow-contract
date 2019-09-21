88f6a96700042ab7b653f83742119cc4c9cd6a44
Finley McIlwaine <finleymcilwaine@gmail.com>
2019-09-21 11:16:34 -0600

    cleared Lib.hs a lil

--- escrow/src/Lib.hs
+++ escrow/src/Lib.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-ignore-interface-pragmas #-}
+
 module Lib
-    ( someFunc
+    (
     ) where
 
 import           Language.PlutusCore
 
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
