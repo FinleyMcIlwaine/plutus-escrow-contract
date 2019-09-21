3de6e36d4e0ac966af916bcb02e14372b72dbc48
Finley McIlwaine <finleymcilwaine@gmail.com>
2019-09-21 11:24:50 -0600

    began Escrow.hs

---
+++ escrow/src/Escrow.hs
@@ -0,0 +1,15 @@
+-- | A guessing game
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts   #-}
+{-# LANGUAGE NoImplicitPrelude  #-}
+{-# LANGUAGE TemplateHaskell    #-}
+{-# LANGUAGE TypeApplications   #-}
+{-# LANGUAGE TypeOperators      #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# OPTIONS_GHC -fno-ignore-interface-pragmas #-}
+module Escrow (
+    
