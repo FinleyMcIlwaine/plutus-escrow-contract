63a75063e80236cd3accdd31cece6ef7f9f7c199
Kegan McIlwaine <kegan.mcilwaine@gmail.com>
2019-09-22 07:33:18 +0000

    Clean up messy test code

--- escrow/test/EscrowTests.hs
+++ escrow/test/EscrowTests.hs
@@ -30,15 +30,15 @@ tests :: TestTree
 tests = testGroup "escrow state machine tests" [
     HUnit.testCaseSteps "initialise - SUCCESS" (runTrace (initialiseTest ()) isRight),
     HUnit.testCaseSteps "initialise, lock - SUCCESS" (runTrace (initialiseLockTest ()) isRight),
-    HUnit.testCaseSteps "lock, sign - SUCCESS" (runTrace (lockSignPay 3 1) isRight),
-    HUnit.testCaseSteps "lock, sign 1x, pay - Funds stay locked" (runTrace (lockSignPay 1 1) isLeft),
-    HUnit.testCaseSteps "lock, sign 2x, pay - Seller receives funds" (runTrace (lockSignPay 2 1) isRight),
-    HUnit.testCaseSteps "lock, sign 3x, pay - Seller receives funds" (runTrace (lockSignPay 3 1) isRight),
-    HUnit.testCaseSteps "lock, dispute 1x, pay - Funds stay" (runTrace (lockDisputePay 1 1) isLeft),
-    HUnit.testCaseSteps "lock, dispute 2x pay - Buyer receives refund" (runTrace (lockDisputePay 2 1) isRight),
-    HUnit.testCaseSteps "lock, dispute 3x pay - Buyer receives refund" (runTrace (lockDisputePay 2 1) isRight),
-    HUnit.testCaseSteps "lock, dispute 1x, sign 2x, pay - Seller receives funds" (runTrace (lockDisputeSignPay 2 1 2 1) isRight),
-    HUnit.testCaseSteps "lock, dispute 2x, sign 1x, pay - Buyer receives refund" (runTrace (lockDisputeSignPay 1 2 1 1 ) isRight)
+    HUnit.testCaseSteps "lock, sign - SUCCESS" (runTrace (lockSignPay 3) isRight),
+    HUnit.testCaseSteps "lock, sign 1x, pay - Funds stay locked" (runTrace (lockSignPay 1) isLeft),
+    HUnit.testCaseSteps "lock, sign 2x, pay - Seller receives funds" (runTrace (lockSignPay 2) isRight),
+    HUnit.testCaseSteps "lock, sign 3x, pay - Seller receives funds" (runTrace (lockSignPay 3) isRight),
+    HUnit.testCaseSteps "lock, dispute 1x, pay - Funds stay" (runTrace (lockDisputePay 1) isLeft),
+    HUnit.testCaseSteps "lock, dispute 2x pay - Buyer receives refund" (runTrace (lockDisputePay 2) isRight),
+    HUnit.testCaseSteps "lock, dispute 3x pay - Buyer receives refund" (runTrace (lockDisputePay 2) isRight),
+    HUnit.testCaseSteps "lock, dispute 1x, sign 2x, pay - Seller receives funds" (runTrace (lockDisputeSignPay 2 1 2) isRight),
+    HUnit.testCaseSteps "lock, dispute 2x, sign 1x, pay - Buyer receives refund" (runTrace (lockDisputeSignPay 1 2 1) isRight)
     ]
 
 runTrace :: EM.EmulatorAction a -> (Either EM.AssertionError a -> Bool) -> (String -> IO ()) -> IO ()
@@ -113,8 +113,8 @@ signPay i = addSignature'' i >=> makePayment''
 disputePay :: (WalletAPI m, WalletDiagnostics m) => Integer -> State -> EM.Trace m State
 disputePay i = dispute'' i >=> makePayment''
 
-lockSignPay :: forall m . (EM.MonadEmulator m) => Integer -> Integer -> m ()
-lockSignPay i j = EM.processEmulated $ do
+lockSignPay :: forall m . (EM.MonadEmulator m) => Integer -> m ()
+lockSignPay i = EM.processEmulated $ do
 
     -- stX contain the state of the contract. See note [Current state of the
     -- contract] in
@@ -122,24 +122,24 @@ lockSignPay i j = EM.processEmulated $ do
     initialise''
     st1 <- lock'' (Ada.adaValueOf 10)
 
-    foldM_ (\st _ -> signPay i st) st1 [1..j]
+    signPay i st1
 
     processAndNotify
     EM.assertOwnFundsEq w2 (Ada.adaValueOf 10)
 
-lockDisputePay :: forall m . (EM.MonadEmulator m) => Integer -> Integer -> m ()
-lockDisputePay i j = EM.processEmulated $ do
+lockDisputePay :: forall m . (EM.MonadEmulator m) => Integer -> m ()
+lockDisputePay i = EM.processEmulated $ do
     initialise''
     st1 <- lock'' (Ada.adaValueOf 10)
-    foldM_ (\st _ -> disputePay i st) st1 [1..j]
+    disputePay i st1
     processAndNotify
     EM.assertOwnFundsEq w1 (Ada.adaValueOf 10)
 
-lockDisputeSignPay :: forall m . (EM.MonadEmulator m) => Integer -> Integer -> Integer -> Integer -> m ()
-lockDisputeSignPay w i j k = EM.processEmulated $ do
+lockDisputeSignPay :: forall m . (EM.MonadEmulator m) => Integer -> Integer -> Integer -> m ()
+lockDisputeSignPay w i j = EM.processEmulated $ do
     initialise''
     st1 <- lock'' (Ada.adaValueOf 10)
-    foldM_ (\st _ -> (dispute'' i >=> addSignature'' j >=> makePayment'') st) st1 [1..k]
+    (dispute'' i >=> addSignature'' j >=> makePayment'') st1
     processAndNotify
     EM.assertOwnFundsEq (EM.Wallet w) (Ada.adaValueOf 10)
 
