9344bc5095693e469cca9ae51ed9f0ace3caed98
Kegan McIlwaine <kegan.mcilwaine@gmail.com>
2019-09-22 02:54:45 +0000

    Basic test

--- escrow/test/EscrowTests.hs
+++ escrow/test/EscrowTests.hs
@@ -28,6 +28,7 @@ import qualified Escrow as ESM
 
 tests :: TestTree
 tests = testGroup "escrow state machine tests" [
+    HUnit.testCaseSteps "initialise - SUCCESS" (runTrace (initialiseTest ()) isRight),
     HUnit.testCaseSteps "lock, sign - SUCCESS" (runTrace (lockSignPay 3 1) isRight),
     HUnit.testCaseSteps "lock, propose, sign 2x, pay - FAILURE" (runTrace (lockSignPay 2 1) isLeft),
     HUnit.testCaseSteps "lock, propose, sign 3x, pay x2 - SUCCESS" (runTrace (lockSignPay 3 2) isRight),
@@ -53,11 +54,11 @@ w2 = EM.Wallet 2
 w3 = EM.Wallet 3
 
 
--- | A payment of 5 Ada to the public key address of wallet 2
+-- | A payment of 10 Ada to the public key address of wallet 2
 payment :: ESM.Payment
 payment =
     ESM.Payment
-        { ESM.paymentAmount    = Ada.adaValueOf 5
+        { ESM.paymentAmount    = Ada.adaValueOf 10
         , ESM.paymentSender    = EM.walletPubKey w1
         , ESM.paymentRecipient = EM.walletPubKey w2
         , ESM.arbitrator       = EM.walletPubKey w3
@@ -83,20 +84,20 @@ initialise'' :: WalletAPI m => EM.Trace m ()
 initialise'' =
 -- instruct all three wallets to start watching the contract address
     traverse_ (\w -> EM.walletAction w initialise') [w1, w2, w3]
--- 
+ 
 lock'' :: (WalletAPI m, WalletDiagnostics m) => Value -> EM.Trace m State
 -- wallet 1 locks the funds
 lock'' value = processAndNotify >> fst <$> EM.walletAction w1 (lock' value (EM.walletPubKey w2) (EM.walletPubKey w3) 10)
--- 
+
 -- proposePayment'' :: (WalletAPI m, WalletDiagnostics m) => State -> EM.Trace m State
 -- proposePayment'' st = processAndNotify >> fst <$> EM.walletAction w2 (proposePayment' st payment)
--- 
+
 addSignature'' :: (WalletAPI m, WalletDiagnostics m) => Integer -> State -> EM.Trace m State
 addSignature'' i inSt = foldM (\st w -> (processAndNotify >> fst <$> EM.walletAction w (addSignature' st))) inSt (take (fromIntegral i) [w1, w2, w3])
--- 
+
 makePayment'' :: (WalletAPI m, WalletDiagnostics m) => State -> EM.Trace m State
 makePayment'' st = processAndNotify >> fst <$> EM.walletAction w3 (makePayment' st)
--- 
+
 signPay :: (WalletAPI m, WalletDiagnostics m) => Integer -> State -> EM.Trace m State
 signPay i = addSignature'' i >=> makePayment''
 
@@ -113,3 +114,8 @@ lockSignPay i j = EM.processEmulated $ do
 
     processAndNotify
     EM.assertOwnFundsEq w2 (scale j (Ada.adaValueOf 5))
+
+initialiseTest () = EM.processEmulated $ do
+    initialise''
+    processAndNotify
+    EM.assertOwnFundsEq w2 (Ada.adaValueOf 10)
